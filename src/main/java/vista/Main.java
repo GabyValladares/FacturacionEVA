f373633d01e5af3948b5d1df1045ab08ae90b7db
kevin2yapu <kevin.yapu809@ist17dejulio.edu.ec>
2026-07-23 11:10:21 -0500

    obtener cliente

--- src/main/java/vista/Main.java
+++ src/main/java/vista/Main.java
@@ -4,7 +4,10 @@
  */
 package vista;
 
+import controlador.ClienteControlador;
 import controlador.ConexionBDD;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -14,7 +17,64 @@ public class Main {
     public static void main(String[] args) {
         ConexionBDD c=new ConexionBDD();
         c.conectar();
-        
     }
+        
+        public static void mostarClientes(){
+            
+            ConexionBDD c = new ConexionBDD();
+        c.conectar();
+
+        ClienteControlador controlador = new ClienteControlador();
+
+        ArrayList<String[]> clientes = controlador.listaCliente();
+
+        // Mostrar en consola
+        for (String[] cliente : clientes) {
+            System.out.println("ID: " + cliente[0]);
+            System.out.println("Nombre: " + cliente[1]);
+            System.out.println("Email: " + cliente[2]);
+            System.out.println("Teléfono: " + cliente[3]);
+            System.out.println("tipo_cliente" + cliente[4]);
+            System.out.println("descuento_vip"+ cliente[5]);
+            System.out.println("-------------------------");
+        }
+
+        // Lista desplegable
+        Object[] nombreClientes = new Object[clientes.size()];
+
+        for (int i = 0; i < clientes.size(); i++) {
+            nombreClientes[i] = clientes.get(i)[1]; // Nombre del cliente
+        }
+
+        if (nombreClientes.length > 0) {
+
+            String clienteSeleccionado = (String) JOptionPane.showInputDialog(
+                    null,
+                    "Seleccione un cliente:",
+                    "Lista de Clientes",
+                    JOptionPane.QUESTION_MESSAGE,
+                    null,
+                    nombreClientes,
+                    nombreClientes[0]
+            );
+
+            JOptionPane.showMessageDialog(
+                    null,
+                    "Cliente seleccionado: " + clienteSeleccionado
+            );
+
+        } else {
+
+            JOptionPane.showMessageDialog(
+                    null,
+                    "No existen clientes registrados."
+            );
+
+        }
+            
+        }
+                
+        
+    
     
 }
