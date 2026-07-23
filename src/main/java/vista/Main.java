aadf1288938802c34805435d6fd878d29886ccba
kevin2yapu <kevin.yapu809@ist17dejulio.edu.ec>
2026-07-23 10:00:12 -0500

    conexion bdd

---
+++ src/main/java/vista/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vista;
+
+import controlador.ConexionBDD;
+
+/**
+ *
+ * @author KEVIN
+ */
+public class Main {
+    public static void main(String[] args) {
+        ConexionBDD c=new ConexionBDD();
+        c.conectar();
+        
+    }
+    
+}
