79e822093460a1746355dd21cbf2df1c4a628134
Santiago <santiago.pulla702@ist17dejulio.edu.ec>
2026-07-23 11:00:21 -0500

    Producto Controlador

--- src/main/java/controlador/ProductoControlador.java
+++ src/main/java/controlador/ProductoControlador.java
@@ -7,6 +7,8 @@ package controlador;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 /**
  *
@@ -22,4 +24,27 @@ public class ProductoControlador {
     //OBTENER RESULTADOS DE LA CONSULTA
     ResultSet resultado;
     
+    public ArrayList<String[]> obtenerProductos() {
+        ArrayList<String[]> lregistros = new ArrayList<>();
+        try {
+            String sentenciaSQL = "select *from productos;";
+            ejecutar = conectado.prepareCall(sentenciaSQL);
+            ResultSet res = ejecutar.executeQuery();
+            
+            while (res.next()) {
+                String[] listaProducto = new String[3];
+                listaProducto[0] = res.getInt("id_producto") + "";
+                listaProducto[1] = res.getString("nombre");
+                listaProducto[2] = res.getString("precio");
+                lregistros.add(listaProducto);
+            }
+             ejecutar.close();
+            conectado.close();
+            return lregistros;
+        } catch (SQLException e) {
+            System.out.println("------" + e);
+        }
+            return lregistros;
+    
+    }
 }
--- src/main/java/vista/Main.java
+++ src/main/java/vista/Main.java
@@ -5,6 +5,9 @@
 package vista;
 
 import controlador.ConexionBDD;
+import controlador.ProductoControlador;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -14,8 +17,26 @@ public class Main {
     public static void main(String[] args) {
         ConexionBDD conx = new ConexionBDD();
         conx.conectar();
-                
-    }
     
-
+    
+        ProductoControlador pc1= new ProductoControlador();
+        ArrayList<String[]> lProducto = pc1.obtenerProductos();
+        Object[] nombre = new Object[lProducto.size()];
+        
+        int i = 0;
+            for(String[] producto : lProducto){ 
+            nombre[i] = producto[1]; 
+            i++;
+        }    
+        
+        String productoElegido = (String) JOptionPane.showInputDialog(null,
+        "Ingrese su producto",
+        "Lista de Productos",
+        JOptionPane.QUESTION_MESSAGE,
+        null,
+        nombre,
+        nombre[0]);
+        JOptionPane.showInternalMessageDialog(null, "Producto seleccionado: " + productoElegido);
+    
+    }
 }
