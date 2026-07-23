406261c6e3a28eac32ea9f36c1ef9c571781389f
kevin2yapu <kevin.yapu809@ist17dejulio.edu.ec>
2026-07-23 09:25:05 -0500

    facturacion

--- src/main/java/controlador/DetalleFacturaControlador.java
+++ src/main/java/controlador/DetalleFacturaControlador.java
@@ -10,6 +10,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
+import modelo.DetalleFactura;
 
 /**
  *
@@ -27,57 +28,40 @@ public class DetalleFacturaControlador {
     ResultSet resultado;
 
     //MÉTODOS DE TRANSACCIONABILIDAD
-    public void insertarDetalleFactura(Pais p) {
-        //1.- UTILIZAR EXCEPCIÓN
-        try {//LANZAR TESTEAR UN CONJUNTO DE CÓDIGO 
-            String sentenciaSQL = "INSERT INTO Paises(nombre,capital)values "
-                    + "('" + p.getNombre() + "','" + p.getCapital() + "');";
-            ejecutar = conectado.prepareCall(sentenciaSQL);
-            //TODA INSERCIÓN DEVUELVE UN ESTADO >0 CUANDO FUE FAVORABLE Y MENOR A O CUANDO NO SE REALIZÓ 
-            int res = ejecutar.executeUpdate();
-            if (res > 0) {
-                JOptionPane.showMessageDialog(null,
-                        "País Creado con éxito");
-                ejecutar.close();
-            } else {
-                JOptionPane.showMessageDialog(null,
-                        "El País no ha sido creado,"
-                        + " revise que los datos ingresados sean correctos");
-            }
-            conectado.close();
+  
 
-        } catch (SQLException e) {
-            //CAPTURAR PARA DARLE UN TRATAMIENTO 
-            JOptionPane.showMessageDialog(null,
-                    "Comuniquese con el Administrador para solicitar ayuda");
-            System.out.println("---------------" + e);
-        }
+    private ConexionBDD conexionBDD;
 
+    public DetalleFacturaControlador() {
+        this.conexionBDD = new ConexionBDD();
     }
 
-    public ArrayList<String[]> obtenerPaises() {
-        ArrayList<String[]> lregistros = new ArrayList<>();
+    public boolean guardarDetalle(int idFactura, DetalleFactura detalle) {
+        String sql = "INSERT INTO detalle_factura (id_factura, id_producto, cantidad, subtotal) VALUES (?, ?, ?, ?)";
+        Connection con = conexionBDD.conectar();
 
-        try {
-            String sentenciaSQL = "select *from paises;";
-            ejecutar = conectado.prepareCall(sentenciaSQL);
-            ResultSet res = ejecutar.executeQuery();
-
-            while (res.next()) {
-                String[] listaPaises = new String[3];
-                listaPaises[0] = res.getInt("id") + "";
-                listaPaises[1] = res.getString("nombre");
-                listaPaises[2] = res.getString("capital");
-                lregistros.add(listaPaises);
-
-            }
-
-            ejecutar.close();
-            conectado.close();
-            return lregistros;
-        } catch (SQLException e) {
-            System.out.println("------" + e);
+        if (con == null) {
+            return false;
         }
-        return lregistros;
+
+        try (PreparedStatement ps = con.prepareStatement(sql)) {
+
+            ps.setInt(1, idFactura);
+            ps.setInt(2, detalle.getProducto().getId());
+            ps.setInt(3, detalle.getCantidad());
+            ps.setDouble(4, detalle.getSubtotal());
+
+            return ps.executeUpdate() > 0;
+
+        } catch (SQLException e) {
+            System.err.println("Error al guardar detalle: " + e.getMessage());
+            return false;
+        } finally {
+            try {
+                if (con != null) con.close();
+            } catch (SQLException ex) {
+                System.err.println("Error al cerrar conexion: " + ex.getMessage());
+            }
+        }
     }
 }
