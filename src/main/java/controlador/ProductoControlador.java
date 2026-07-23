761ccc5e98fa9cebb6b0a359538e8cc37062d6e4
hafitmarroquin300-code <hafit.marroquin300@ist17dejulio.edu.ec>
2026-07-23 11:01:15 -0500

    Producto controlador

--- src/main/java/controlador/ProductoControlador.java
+++ src/main/java/controlador/ProductoControlador.java
@@ -4,10 +4,54 @@
  */
 package controlador;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 /**
  *
  * @author Asus
  */
 public class ProductoControlador {
+     //INSTANCIAR LA CONEXIÓN A LA BASE DE DATOS
+    ConexionBDD conectar = new ConexionBDD();
+    //CLASE QUE ME PERMITA CONECTARME DIRECTAMENTE A MYSQL
+    Connection conectado = (Connection) conectar.conectar();
+    //CLASE QUE ME PERMITE EJECUTAR MI SENTENCIA SQL
+    PreparedStatement ejecutar;
+    //OBTENER RESULTADOS DE LA CONSULTA
+    ResultSet resultado;
     
+    
+    public ArrayList<String[]> obtenerProductos() {
+        ArrayList<String[]> lregistros = new ArrayList<>();
+
+        try {
+            String sentenciaSQL = "SELECT * FROM productos";
+            ejecutar = conectado.prepareCall(sentenciaSQL);
+            ResultSet res = ejecutar.executeQuery();
+
+            while (res.next()) {
+                String[] listaProductos = new String[3];
+
+                listaProductos[0] = String.valueOf(res.getInt("id_producto"));
+                listaProductos[1] = res.getString("nombre_producto");
+                listaProductos[2] = res.getString("precio_producto");
+
+                lregistros.add(listaProductos);
+            }
+
+            res.close();
+            ejecutar.close();
+            conectado.close();
+
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+        return lregistros;
+
+    }
 }
--- src/main/java/vista/Main.java
+++ src/main/java/vista/Main.java
@@ -5,14 +5,40 @@
 package vista;
 
 import controlador.ConexionBDD;
+import controlador.ProductoControlador;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Asus
  */
 public class Main {
+
     public static void main(String[] args) {
-        ConexionBDD c=new ConexionBDD();
+        ConexionBDD c = new ConexionBDD();
         c.conectar();
+
+        ProductoControlador pc = new ProductoControlador();
+
+        System.out.println(pc.obtenerProductos());
+
+        ArrayList<String[]> lista = pc.obtenerProductos();
+
+        String[] opciones = new String[lista.size()];
+        JOptionPane.showInputDialog(null, "Seleccionar", "Lista desplegable",
+                 JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[0]);
+        
+        String paisSeleccionado = (String) JOptionPane.showInputDialog(
+        null,
+        "Escoja su país",
+        "PAÍSES",
+        JOptionPane.QUESTION_MESSAGE,
+        null,
+        opciones,
+        opciones[0]
+);
+
+        System.out.println(paisSeleccionado);
     }
 }
