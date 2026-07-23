63e54abdebf44f0e03b8cb05dc6b26acf631f41a
omarmoreta539-ctrl <omar.moreta539@ist17dejulio.edu.ec>
2026-07-23 10:00:49 -0500

    Conexion BDD

--- src/main/java/controlador/DetalleFacturaControlador.java
+++ src/main/java/controlador/DetalleFacturaControlador.java
@@ -27,33 +27,33 @@ public class DetalleFacturaControlador {
     ResultSet resultado;
 
     //MÉTODOS DE TRANSACCIONABILIDAD
-    public void insertarDetalleFactura(Pais p) {
-        //1.- UTILIZAR EXCEPCIÓN
-        try {//LANZAR TESTEAR UN CONJUNTO DE CÓDIGO 
-            String sentenciaSQL = "INSERT INTO Paises(nombre,capital)values "
-                    + "('" + p.getNombre() + "','" + p.getCapital() + "');";
-            ejecutar = conectado.prepareCall(sentenciaSQL);
-            //TODA INSERCIÓN DEVUELVE UN ESTADO >0 CUANDO FUE FAVORABLE Y MENOR A O CUANDO NO SE REALIZÓ 
-            int res = ejecutar.executeUpdate();
-            if (res > 0) {
-                JOptionPane.showMessageDialog(null,
-                        "País Creado con éxito");
-                ejecutar.close();
-            } else {
-                JOptionPane.showMessageDialog(null,
-                        "El País no ha sido creado,"
-                        + " revise que los datos ingresados sean correctos");
-            }
-            conectado.close();
-
-        } catch (SQLException e) {
-            //CAPTURAR PARA DARLE UN TRATAMIENTO 
-            JOptionPane.showMessageDialog(null,
-                    "Comuniquese con el Administrador para solicitar ayuda");
-            System.out.println("---------------" + e);
-        }
-
-    }
+//    public void insertarDetalleFactura(Pais p) {
+//        //1.- UTILIZAR EXCEPCIÓN
+//        try {//LANZAR TESTEAR UN CONJUNTO DE CÓDIGO 
+//            String sentenciaSQL = "INSERT INTO Paises(nombre,capital)values "
+//                    + "('" + p.getNombre() + "','" + p.getCapital() + "');";
+//            ejecutar = conectado.prepareCall(sentenciaSQL);
+//            //TODA INSERCIÓN DEVUELVE UN ESTADO >0 CUANDO FUE FAVORABLE Y MENOR A O CUANDO NO SE REALIZÓ 
+//            int res = ejecutar.executeUpdate();
+//            if (res > 0) {
+//                JOptionPane.showMessageDialog(null,
+//                        "País Creado con éxito");
+//                ejecutar.close();
+//            } else {
+//                JOptionPane.showMessageDialog(null,
+//                        "El País no ha sido creado,"
+//                        + " revise que los datos ingresados sean correctos");
+//            }
+//            conectado.close();
+//
+//        } catch (SQLException e) {
+//            //CAPTURAR PARA DARLE UN TRATAMIENTO 
+//            JOptionPane.showMessageDialog(null,
+//                    "Comuniquese con el Administrador para solicitar ayuda");
+//            System.out.println("---------------" + e);
+//        }
+//
+//    }
 
     public ArrayList<String[]> obtenerPaises() {
         ArrayList<String[]> lregistros = new ArrayList<>();
